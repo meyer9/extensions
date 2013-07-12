c2d6f04d30dcebc34dc102a814f010699de5f8bd
Matt Murfitt <murfitt@gmail.com>
2013-07-12 01:46:31 +0000

    Added io_dm3 plugin. Example io plugin. Reads and writes DM3 files.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from dm3_image_utils import load_image, save_image
+import Application
+
+Application.app.import_export_manager.register_io(
+    name="DM3",
+    extensions=["dm3"],
+    load_func=load_image,
+    save_func=save_image)
