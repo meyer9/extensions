09106447c1073bb824a9107254aee81440e88311
Chris Meyer <cmeyer1969@gmail.com>
2013-10-21 17:06:13 +0000

    Refactor import/export to support more file types.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,18 @@ from dm3_image_utils import load_image, save_image
 
 from nion.swift import ImportExportManager
 
-ImportExportManager.ImportExportManager().register_io(
-    name="DM3",
-    extensions=["dm3"],
-    load_func=load_image,
-    save_func=save_image)
+class DM3ImportExportHandler(ImportExportManager.ImportExportHandler):
+
+    def __init__(self):
+        super(DM3ImportExportHandler, self).__init__("DigitalMicrograph 3", ["dm3"])
+
+    def read_data(self, extension, f):
+        load_image(f)
+
+    def can_write(self, data_item, extension):
+        return len(data_item.spatial_shape) == 2
+
+    def write_data(self, data, extension, f):
+        save_image(data, f)
+
+ImportExportManager.ImportExportManager().register_io_handler(DM3ImportExportHandler())
